7719db71e09cb913883078109bd335b19106dfe1
rot-ciV <fagundes.nvictor@gmail.com>
2025-06-14 20:06:50 -0300

    Data Base um pouco mais complexo

--- db.pl
+++ db.pl
@@ -1,14 +1,56 @@
-artropodes(formiga).
-artropodes(aranha).
-artropodes(siri).
-artropodes(largata).
-artropodes(lacraia).
-artropodes(piolho de cobra).
-artropodes(escorpião).
-artropodes(camarao).
-
-inseto(artropodes, 3_pernas)
+% Fatos
 
 
-does_formiga_dances :- formiga(artropodes),
-    write ('formiga n danca doido')
+artropode(formiga).
+artropode(aranha).
+artropode(siri).
+artropode(lagarta).
+artropode(lacraia).
+artropode(piolho_de_cobra).
+artropode(escorpiao).
+artropode(camarao).
+artropode(centopeia). 
+
+
+% % == REGRAS DE CLASSIFICAÇÃO == 
+
+
+inseto(X) :- 
+    artropode(X),
+    member(X, [formiga, lagarta]),
+    explicacao_inseto(X).
+
+aracnideo(X) :- 
+    artropode(X),
+    member(X, [aranha, escorpiao]).
+
+crustaceo(X) :- 
+    artropode(X),
+    member(X, [siri, camarao]).
+
+quilopodes(X) :- 
+    artropode(X),
+    member(X, [lacraia, centopeia]).
+
+diplopode(X) :- 
+    artropode(X),
+    member(X, [piolho_de_cobra]).
+
+predador(X) :- member(X, [aranha, escorpiao, siri, lacraia, camarao]).
+
+
+% % === REGRAS DE EXPLICAÇÕES ESPECÍFICAS ===
+
+
+explicacao_inseto(formiga) :-
+    write('A formiga é um inseto. Isso é por conta das seguintes características:'), nl,
+    write('- 6 patas.'), nl,
+    write('- Corpo dividido em: cabeça, tórax e abdômen.'), nl,
+    write('- Exoesqueleto de quitina.'), nl.
+
+explicacao_inseto(lagarta) :-
+    write('Mesmo não parecendo, a lagarta é um inseto! Isso é por conta das seguintes características:'), nl,
+    write('- 6 patas verdadeiras. Isso mesmo, ela apenas aparenta ter várias patas, todas que estão em seu abdomen são consideradas pseudopatas!'), nl,
+    write('- Corpo dividido em: cabeça, tórax e abdômen'), nl,
+    write('- Exoesqueleto de quitina'), nl,
+    write('Curiosidade: A lagarta é a fase larval de insetos como borboletas e mariposas.'), nl.
