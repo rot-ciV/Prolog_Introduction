516bb640c7bae373f44e3b938f7825ae72613528
rot-ciV <fagundes.nvictor@gmail.com>
2025-06-14 14:31:23 -0300

    espaço de estudos de VIctor

---
+++ db.pl
@@ -0,0 +1,12 @@
+artropodes(formiga).
+artropodes(aranha).
+artropodes(siri).
+artropodes(largata).
+artropodes(lacraia).
+artropodes(piolho de cobra).
+artropodes(escorpião).
+artropodes(camarao).
+
+inseto(artropodes, 3_pernas)
+
+
